e8c0ecdfe481c992a0cd5a1a5370278d6f328c9a
dapod93 <davidliois50@gmail.com>
2025-03-19 13:02:44 +0700

    update: files
    Signed-off-by: dapod93 <davidliois50@gmail.com>

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-var express = require("express");
-var cors = require("cors");
 require("dotenv").config();
+
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const express = require("express");
+const multer = require("multer");
+const path = require('path');
 
 var app = express();
 
+
+const uploadDir  = path.join(__dirname, "uploads")
+const uploader = multer({storage: });
+
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/public", express.static(process.cwd() + "/public"));
 
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
+
+app.post("/api/fileanalyse", upload, async (req, res) => {});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
