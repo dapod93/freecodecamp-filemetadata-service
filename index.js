dbbb31ecc5c1dc219ac311652664fdf1ff18341b
dapod93 <davidliois50@gmail.com>
2025-03-19 13:13:15 +0700

    update: files
    Signed-off-by: dapod93 <davidliois50@gmail.com>

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ require("dotenv").config();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const express = require("express");
+const fs = require("fs");
 const multer = require("multer");
 const path = require("path");
 
 var app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/public", express.static(process.cwd() + "/public"));
 
 const uploadDir = path.join(__dirname, "uploads");
 const storage = multer.diskStorage({
@@ -20,15 +25,18 @@ const storage = multer.diskStorage({
 });
 const uploader = multer({ storage: storage });
 
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/public", express.static(process.cwd() + "/public"));
+const checkUploadLimitMiddleware = (req, res, next) => {
+  if (fs.readdirSync(uploadDir).filter((file) => fs.statSync(path.join(uploadDir, file)).isFile()).length >= 5) {
+    return res.json({ error: "limit reached" });
+  }
+  next();
+};
 
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.post("/api/fileanalyse", uploader.single("upfile"), (req, res) => {
+app.post("/api/fileanalyse", checkUploadLimitMiddleware, uploader.single("upfile"), (req, res) => {
   if (!req.file) {
     return res.json({ error: "no file uploaded" });
   }
